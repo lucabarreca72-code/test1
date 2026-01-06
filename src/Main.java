53a0acce56912ba9bcbd8297bbc236d782be39be
Jenki <lucabarreca72@gmail.com>
2026-01-06 19:54:38 +0100

    init commit

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,22 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
 
+        // PREZZI
         double hamburger = 12.99;
         double fries = 4.99;
         double soda = 2.99;
         double total;
 
-        int quantity1;
-        int quantity2;
-        int quantity3;
+        //QUANTITÁ
+        int quantity1 ;
+        int quantity2 = 0;
+        int quantity3 = 0;
+
+        //OPZIONI
         boolean fries1;
         boolean soda1;
         int code;
         code = random.nextInt(1, 99);
-
-
 
         System.out.println("Hello and welcome to the Krusty Krab, here's the menu:\nHamburger: 12.99 \nFries: 4.99 \nSoda: 2.99 ");
 
@@ -37,30 +39,26 @@ public class Main {
             System.out.println("ok");
         }
 
-        System.out.println("would you like a soda? (ture/false): ");
+        System.out.print("would you like a soda? (ture/false): ");
         soda1 = scanner.nextBoolean();
         if (soda1){
-            System.out.println("how many?");
+            System.out.print("how many? ");
             quantity3 = scanner.nextInt();
         }
         else {
             System.out.println("Perfect, thank you for your order your number is: " + code) ;
         }
 
-        System.out.println("Perfect, thank you for your order your total is: " ) + total + ("your number is: ") + code;
-
-        double hamburgerTotal = hamburger * quantity1;
-
-        /*double friesTotal = fries * quantity2;
-        double sodaTotal = soda * quantity3;
-        SE TOGLI IL COMMENTO NOTI CHE quantity2 E quantity3 NON SONO INIZIALIZZATE MA IN TEORIA DOVREBBERO
-        ESSERE INIZIALIZZATE IN RIGA 38 E RIGA 47, COME MAI??
-        E POI CHIARAMENTE total PERCHÉ LO AVREI FATTO DOPO.
-
-
-        */
-
         scanner.close();
+
+        //TOTALE QUANTITÁ
+        double hamburgerTotal = hamburger * quantity1;
+        double friesTotal = fries * quantity2;
+        double sodaTotal = soda * quantity3;
+
+        total = hamburgerTotal + friesTotal + sodaTotal;
+
+        System.out.println("\nYour total is: " + total + " and your number is: " + code);
 
     }
 }
