560ed10c7cb366b6e7c42c67d3eb91001fbc1eac
lucabarreca72-code <lucabarreca72@gmail.com>
2026-01-10 19:14:56 +0100

    pulizia generale aggiunta di sconto in % implemento di printf

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.MathContext;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,28 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
 
-        // PREZZI
+        // PRICES
         double hamburger = 12.99;
         double fries = 4.99;
         double soda = 2.99;
         double total = 0;
-        char currency = '€';
 
-        //QUANTITÁ
+        //QUANTITY
         int quantity1 = 0;
         int quantity2 = 0;
         int quantity3 = 0;
 
-        //OPZIONI
+        //OPTIONS
         boolean fries1;
         boolean soda1;
         int code;
         code = random.nextInt(1, 99);
 
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello and welcome to the Krusty Krab, here's the menu:\nHamburger: 12.99 \nFries: 4.99 \nSoda: 2.99 ");
-
-        Scanner scanner = new Scanner(System.in);
 
         System.out.print("So how many hamburgers would you like? (if none enter 0): ");
         quantity1 = scanner.nextInt();
@@ -48,21 +47,26 @@ public class Main {
             quantity3 = scanner.nextInt();
         }
         else {
-            System.out.println() ;
+            System.out.println();
         }
 
         scanner.close();
 
-        //TOTALE QUANTITÁ
+        //QUANTITY TOTAL
         double hamburgerTotal = hamburger * quantity1;
         double friesTotal = fries * quantity2;
         double sodaTotal = soda * quantity3;
-
         total = hamburgerTotal + friesTotal + sodaTotal;
+        double discount = (total * 15) / 100;
+        double total2 = total - discount;
 
-        System.out.println("\nGreat thank's for your order,\nthe total is: " + total + currency + "\nand your number is: " + code);
+        if (total > 20.99){
+            System.out.printf("Wow so lucky, you got a %.2f€ discount, \nyour total is: %.2f€ \nand this is your order number: %d", discount, total2, code);
+        }
+        else {
+            System.out.printf("Great thank's for your order,\nthe total is: %.2f€ \nand this is your order number: %d", total, code);
+        }
 
-        // test 2
 
     }
 }
